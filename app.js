56a5c609b424ad681437a3aeebb2944502d62664
karanbirsandhu <56524111+karanbirsandhu@users.noreply.github.com>
2021-06-19 16:59:22 -0400

    Online deployer website

--- app.js
+++ app.js
@@ -1,41 +1,192 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const getDate=require(__dirname+"/date.js")
+const getDate = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
-let workitems=[];
-let itemzip=["Buy Food","Cook Food","Eat Food"];
+// connecting mongoose
+mongoose.connect("mongodb+srv://karan1993sandhu:Amrit1313@firstcluster.n6av3.mongodb.net/toDoListDB?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology:true
+});
 
+// let workitems=[];
+let itemzip = [];
+
+// Mongoose schema
+
+const itemSchema = {
+  name: String,
+};
+// creating a model
+
+const Item = mongoose.model("Item", itemSchema);
+
+// creating item docs
+const firstItem = new Item({
+  name: "Welcome to your todo list!!",
+});
+
+const secItem = new Item({
+  name: "Hit the + button to add new items",
+});
+
+const thirdItem = new Item({
+  name: "<-- Hit this to delete an item",
+});
+
+const defaultItems = [firstItem, secItem, thirdItem];
+
+const listSchema={
+  name:String,
+  items:[itemSchema] //creates itemschema based array
+}
+
+const List=mongoose.model("List",listSchema)
 
 app.get("/", (req, res) => {
-  day=getDate.getDate();  
-  res.render("list", { day: day, todo: itemzip });
+  // check if default items are there or not to insert
+
+  // fetching items from DBs
+
+  Item.find((err, results) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (results.length == 0) {
+        Item.insertMany(defaultItems, (err) => {
+          // inserting docs to DB
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("default items added to DB");
+          }
+        });
+        res.redirect("/");
+      } else {
+        res.render("list", { day: "Today", todo: results });
+      }
+    }
+  });
+
+
 });
 
-app.get("/work",(req,res)=>{
-  res.render("list",{day:"Work", todo:workitems})
+
+app.get("/:listtype",(req,res)=>{
+  const customlisttype=req.params.listtype
+
+  List.findOne({name:customlisttype},(err,results)=>{
+    if (!results){
+      const list=new List ({
+        name:customlisttype,
+        items:defaultItems
+    
+      })
+      
+    
+      list.save(()=>{
+        res.redirect("/"+customlisttype)
+      })
+      
+    }
+    else{
+    res.render("list",{day:results.name,todo:results.items})
+    }
+  })
+
+  
 })
 
-app.post("/", (req, res) => {
-console.log(req.body)
-todo = String(req.body.todo);
-if (req.body.btn=="Work"){
-  workitems.push(todo)
-  res.redirect("/work")
-}
-else{
-  itemzip.push(todo);
 
+
+
+app.post("/", (req, res) => {
+  const itemName = req.body.todo;
+
+  const newItem = new Item({
+    name: itemName,
+    items:defaultItems
+  });
+  newItem.save();
   res.redirect("/");
-}
 });
+
+
+
+
+
+app.post("/del",(req,res)=>{
+  const itemId=req.body.del
+  const listName=req.body.listName
+
+  
+  if(listName==="Today"){
+    Item.findByIdAndDelete(itemId,(err)=>{
+      if(err){
+        console.log(err)
+      }
+      res.redirect('/')
+    }); 
+  }
+  else{
+    List.findOneAndUpdate({"name":listName},{$pull:{ items: {"_id":itemId}}},{new:true},(err,response)=>{
+      if(err){
+        console.log(err)
+      }
+      else{
+        
+        res.redirect("/"+listName)
+      }
+
+    })
+   
+    
+  }
+});
+
+// Dynamic API always placed at bottom
+app.post("/:listtype",(req,res)=>{
+  const listtypename = req.params.listtype;
+
+  const itemName = req.body.todo;
+
+  const newItem = new Item({
+    name: itemName
+  });
+
+  if (listtypename=="Today"){
+    newItem.save();
+    res.redirect('/')
+
+  }
+  else{
+    List.findOne({name:listtypename},(err,results)=>{
+      if(err){
+        console.log(err)
+      }
+      if(!results){
+        console.log("nothingfound"+listtypename)
+      }
+      else{
+
+      results.items.push(newItem)
+      results.save(()=>{res.redirect("/"+ listtypename)});
+      }
+      
+    })
+  }
+
+  
+
+})
 
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
